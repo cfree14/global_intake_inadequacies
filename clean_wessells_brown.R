c9c9e668a56d7ddb021765d4c0390b4ad84a0007
Chris Free <cfree14@gmail.com>
2022-12-14 12:58:03 -0800

    added zin absoprtion data

---
+++ clean_wessells_brown.R
@@ -0,0 +1,40 @@
+
+
+# Clear workspace
+rm(list = ls())
+
+# Setup
+################################################################################
+
+# Packages
+library(GENuS)
+library(tidyverse)
+
+# Directories
+datadir <- "data/wessells_brown"
+
+# Get data
+data_orig <- readxl::read_excel(file.path(datadir, "Wessells_Brown_2012_Table_S2.xls"))
+
+
+# Format data
+################################################################################
+
+# Format data
+data <- data_orig %>%
+  # Rename
+  janitor::clean_names("snake") %>%
+  # Simplify
+  select(country, year, estimated_fractional_absorption) %>%
+  # Format year
+  mutate(year=gsub('b', "", year) %>% as.numeric()) %>%
+  # Add ISO3
+  mutate(iso3=countrycode::countrycode(country, "country.name", "iso3c")) %>%
+  # Get most recent
+  group_by(country) %>%
+  arrange(country, desc(year)) %>%
+  slice(1) %>%
+  ungroup()
+
+# Export
+saveRDS(data, file=file.path(datadir, "wessells_brown_2012_zinc_absorption.Rds"))
