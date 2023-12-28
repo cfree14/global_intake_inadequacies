450b0637d38b34569c588baf67cb9ca40e4cf727
cfree14 <cfree14@gmail.com>
2023-12-28 08:58:59 -0500

    subregion work

--- code/gdd_approach/figures/FigS12_subregion_map.R
+++ code/gdd_approach/figures/FigS12_subregion_map.R
@@ -36,9 +36,9 @@ world_sm <- world_sm_orig %>%
                              iso3=="XKX" ~ "Southern Europe",
                              iso3=="TWN" ~ "Eastern Asia",
                              iso3=="ATA" ~ "", # Antarctica
-                             iso3=="CYN" ~ "", # Northern Cyprus
-                             iso3=="KOS" ~ "", # Kosovo
-                             iso3=="SOL" ~ "", # Solomon Islands
+                             iso3=="CYN" ~ "Western Asia", # Northern Cyprus
+                             iso3=="KOS" ~ "Southern Europe", # Kosovo
+                             iso3=="SOL" ~ "Melanesia", # Solomon Islands
                              T ~ subregion)) %>%
   mutate(subregion=recode(subregion,
                           "Latin America and the Caribbean"="Latin America / Caribbean",
@@ -54,27 +54,27 @@ world_centers <- world_centers_orig %>%
   mutate(subregion=case_when(iso3=="CHI" ~ "Western Europe",
                              iso3=="XKX" ~ "Southern Europe",
                              iso3=="TWN" ~ "Eastern Asia",
-                             # iso3=="ATC" ~ "",
-                             # iso3=="BAC" ~ "",
-                             # iso3=="BJN" ~ "",
-                             # iso3=="CLP" ~ "",
-                             # iso3=="CNM" ~ "",
-                             # iso3=="CSI" ~ "",
-                             # iso3=="CYN" ~ "",
-                             # iso3=="ESB" ~ "",
-                             # iso3=="ESC" ~ "",
-                             # iso3=="IOA" ~ "",
-                             # iso3=="KAB" ~ "",
-                             # iso3=="KAS" ~ "",
-                             # iso3=="KOS" ~ "",
-                             # iso3=="NJM" ~ "",
-                             # iso3=="PAZ" ~ "",
-                             # iso3=="PGA" ~ "",
-                             # iso3=="PMD" ~ "",
-                             # iso3=="SCR" ~ "",
-                             # iso3=="SER" ~ "",
-                             # iso3=="USG" ~ "",
-                             # iso3=="WSB" ~ "",
+                             iso3=="ATC" ~ "Australia and New Zealand", # Ashmore and Cartier Islands
+                             iso3=="BAC" ~ "Sub-Saharan Africa", # Ascension
+                             iso3=="BJN" ~ "Latin America and the Caribbean", # Bajo Nuevo Bank (Petrel Is.)
+                             iso3=="CLP" ~ "Latin America and the Caribbean", # Clipperton Island
+                             iso3=="CNM" ~ "Western Asia", # Cyprus No Mans Area
+                             iso3=="CSI" ~ "Australia and New Zealand", # Coral Sea Islands
+                             iso3=="CYN" ~ "Western Asia", # Northern Cyprus
+                             iso3=="ESB" ~ "Western Asia", # Dhekelia Sovereign Base Area
+                             iso3=="ESC" ~ "Southern Europe", # Canary Islands
+                             iso3=="IOA" ~ "South-eastern Asia", # Indian Ocean Territories
+                             iso3=="KAB" ~ "Central Asia", # Baykonur Cosmodrome
+                             iso3=="KAS" ~ "Southern Asia", # Siachen Glacier
+                             iso3=="KOS" ~ "Southern Europe", # Kosovo
+                             iso3=="NJM" ~ "Northern Europe", # Jan Mayen
+                             iso3=="PAZ" ~ "Southern Europe", # Azores
+                             iso3=="PGA" ~ "South-eastern Asia", # Spratly Islands
+                             iso3=="PMD" ~ "Southern Europe", # Madeira
+                             iso3=="SCR" ~ "South-eastern Asia", # Scarborough Reef
+                             iso3=="SER" ~ "Latin America and the Caribbean", # Serranilla Bank
+                             iso3=="USG" ~ "Latin America and the Caribbean", # US Naval Base Guantanamo Bay
+                             iso3=="WSB" ~ "Western Asia", # Akrotiri Sovereign Base Area
                              T ~ subregion)) %>%
   mutate(subregion=recode(subregion,
                           "Latin America and the Caribbean"="Latin America / Caribbean",
@@ -107,11 +107,11 @@ my_theme <-  theme(legend.text=element_text(size=5),
 g <- ggplot(world_sm, aes(fill=subregion)) +
   geom_sf(lwd=0.2, color="grey30") +
   geom_point(data=world_centers, mapping=aes(x=long_dd, y=lat_dd, fill=subregion),
-             color="grey30", size=1.5, pch=21) +
+             color="grey30", size=1.8, pch=21) +
   # Labels
   labs(x="", y="") +
   # Legend
-  scale_fill_discrete(name="UN subsubregion") +
+  scale_fill_discrete(name="UN subregion", na.translate = F) +
   # Crop
   coord_sf(ylim=c(-52, 80), expand = T) +
   # Theme
@@ -120,7 +120,7 @@ g
 
 
 # Export
-ggsave(g, filename=file.path(plotdir, "FigS12_subsubregion_map.png"),
+ggsave(g, filename=file.path(plotdir, "FigS12_subregion_map.png"),
        width=6.5, height=3.75, units="in", dpi=600)
 
 
